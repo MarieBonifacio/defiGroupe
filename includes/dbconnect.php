3a164f23a74b8cdcafb320dbd3f8add602ebfeb3
Byidi <sajot.vincent@gmail.com>
2019-08-22 09:59:42 +0200

    Fix require config.php in dbconnect

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,6 +1,6 @@
 
 <?php
-require('./includes/config.php');
+require(dirname(__DIR__).'/includes/config.php');
 
 try {
     $dbh = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
