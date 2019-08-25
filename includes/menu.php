87b14a896b0c93da897df9668cb13f1c7589b294
Byidi <contact@byidi.fr>
2019-08-25 16:44:59 +0200

    fix menu

--- includes/menu.php
+++ includes/menu.php
@@ -14,35 +14,37 @@
                     <?php
                         foreach($nav as $value){
                             if($value == "categories"){
-                                echo '<li><a href="'.$value.'.php">'.$value.'</a></li>';
+                                echo '<li class="submenuTitle" rel="cat">'.$value.'';
                                 require ("./includes/dbconnect.php");
                                 $sql = "SELECT * FROM categories ORDER BY ordre";
                                 $select = $dbh->query($sql);
-                                while($row = $select->fetch()){
-                                    echo '<li><a class="submenu" href="'.$row["nom"].'.php">'.$row["nom"].'</a></li>';
-                                } 
-                                
-
+                                echo '<ul class="submenuroll" rel="cat">';
+                                    while($row = $select->fetch()){
+                                        echo '<li><a class="submenu" href="./category.php?id='.$row["id"].'.php">'.$row["nom"].'</a></li>';
+                                    }
+                                echo '</ul></li>';
                             }elseif($value == "compte"){
                                 if(isset($_SESSION['userLog'])){
                                     echo '<li><a href="profil.php">Profil</a></li>';
                                     echo '<li><a href="deconnexion.php">Deconnexion</a></li>';
                                 }else{
-                                    echo '<li><a href="./connexion.php">Connexion</a></li>';
-                                    echo '<li><a href="./inscription.php">Inscription</a></li>';
-                                    
-                                } 
-                            }else {
+                                    echo '<li><a href="./login.php">Connexion</a></li>';
+                                    echo '<li><a href="./signup.php">Inscription</a></li>';
+
+                                }
+                            }elseif($value == "accueil"){
+                                echo '<li><a href="index.php">'.$value.'</a></li>';
+                            } else {
                                 echo '<li><a href="'.$value.'.php">'.$value.'</a></li>';
                             }
                         }
                         ?>
                         <button class="opensearch"><i class="material-icons">search</i></button>
-                        
+
                         <div class="searchbar">
                             <input type="search"><button class="search"><i class="material-icons">search</i></button>
                         </div>
                 </ul>
-                
+
             </div>
     </div>
