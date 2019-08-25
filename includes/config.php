7abbaa31cde7401148fec8a6d234bdfadb8e51cd
Byidi <contact@byidi.fr>
2019-08-25 23:06:45 +0200

    config

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Menu navigation
+$nav = ["accueil" , "categories" , "compte"];
+
+
+//Base de données
+$dbhost = "webserv-db";
+$dbname = "lunettes";
+
+//Utilisateur
+
+$dbuser = "dbuser";
+$dbpass = "dbpass";
+
+//Inscription / Compte
+
+
+
+?>
