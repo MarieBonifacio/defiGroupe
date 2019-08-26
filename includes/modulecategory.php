fe4417ffe4e1c238fc8ffe8bf38deb009a3629d0
Byidi <sajot.vincent@gmail.com>
2019-08-26 14:02:50 +0200

    Fix lien module categorie index

--- includes/modulecategory.php
+++ includes/modulecategory.php
@@ -2,12 +2,12 @@
     <div class="moduleCat">
         <div class="highTech">
             <p class="nomcat">High-Tech</p>
-            <a href="categoryHightech.php" ><img class="imgUne" src="./images/cuvette-de-luxe-noir.jpg"></a>
+            <a href="./category.php?id=1" ><img class="imgUne" src="./images/cuvette-de-luxe-noir.jpg"></a>
             <div class="texteCategory"></div>
         </div>
         <div class="deLuxe">
             <p class="nomcat">Luxe</p>
-            <a href="categoryLuxe.php"><img class="imgDeux" src="./images/amaze-lunette.jpg"></a>
+            <a href="./category.php?id=2"><img class="imgDeux" src="./images/amaze-lunette.jpg"></a>
             <div class="texteCategory"></div>
         </div>
     </div>
